03f7da17fbf10aea0eac367101c9e61cab913ddf
Raneem Mohamed <98766927+Roneri8iny@users.noreply.github.com>
2023-12-06 02:48:49 +0200

    Last Update main

--- main.py
+++ main.py
@@ -1,84 +1,103 @@
 import numpy as np
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
+import ECLAT_implementation
 
-file_path = r'D:\Uni Related\Data Mining\Horizontal_Format.xlsx'
+file_path = r'D:\Uni Related\Data Mining\trial.xlsx'
 
 df = pd.read_excel(file_path)
 
 # Create a DataFrame
-df = pd.DataFrame(df)
-
-frequent_itemsets = {}
+input_df = pd.DataFrame(df)
 
 
-# Function to perform pruning
-def support_pruning(df_before_pruning, min_sup):
-    for index, row in df_before_pruning.iterrows():
-        count = 0  # Reset count for each row
-        for col in df_before_pruning.columns[1:]:  # Start from the second column to exclude 'items'
-            if row[col] == 1:  # Check if the value is True
-                count += 1
-        if count < min_sup:
-            # Drop the row if the count is less than min_sup
-            df_before_pruning = df_before_pruning.drop(index)
-    # Reset the index after dropping rows
-    df_before_pruning.reset_index(drop=True, inplace=True)
-    df_pruned = df_before_pruning
-    return df_pruned
+def main():
+    # Detect Type
+    detected_type = ECLAT_implementation.check_type_of_data(input_df)
+    if detected_type == 'Horizontal':
+        vertical_df = ECLAT_implementation.horizontal_to_vertical(input_df)
+    else:
+        vertical_df = ECLAT_implementation.modify_vertical(input_df)
+    # take input
+    min_sup = int(input("Enter Minimum Support Count"))
+    min_conf = int(input("Enter min Confidence in Percentage"))
+    min_conf = min_conf / 100
+    # support pruning 1st time
+    df_after_pruning = ECLAT_implementation.support_pruning(vertical_df, min_sup)
+    # insert in frequent item-sets
+    ECLAT_implementation.insert_frequent_itemsets(df_after_pruning)
+    # generate candidate item-sets of length 2
+    df_after_generating_l2 = ECLAT_implementation.generate_candidate_itemsets_l2(df_after_pruning)
+    temp_df = df_after_generating_l2
+    result = ECLAT_implementation.mine_frequent_itemsets(temp_df, min_sup)
+    print(ECLAT_implementation.frequent_itemsets)
+    ECLAT_implementation.generate_association_rules(min_conf)
+    print('Strong Rules')
+    print(ECLAT_implementation.print_strong_rules())
+
+    #ECLAT_implementation.calculate_lift()
+    #ECLAT_implementation.extract_unique_combinations()
+    #ECLAT_implementation.calculate_lift()
+    #print(ECLAT_implementation.all_combinations_list)
+    # print(result)
+    #for key, value in ECLAT_implementation.association_rules.items():
+        #print(f'{key}: {value}')
 
 
-def insert_frequent_itemsets(df_frequent_itemsets):
-    for index, row in df_frequent_itemsets.iterrows():
-        count = 0
-        for col in df_frequent_itemsets.columns[1:]:  # Start from the second column to exclude 'items'
-            if row[col]:  # Check if the value is True
-                count += 1
-        frequent_itemsets[df_frequent_itemsets.iloc[index, 0]] = count
-
-
-def generate_frequent_itemsets(df_pruned):
-    df_after_generation = pd.DataFrame()
-    # df_of_items['items'] = df_after_pruning['items']
-    # num_rows = len(df_after_pruning)
-    items = df_pruned['items'].tolist()
-    # combined_values = []
-    combinations = []
-    for i in range(len(items)):
-        for j in range(i + 1, len(items)):
-            combinations.append(items[i] + items[j])
-    df_after_generation['items'] = combinations
-
-    return df_after_generation
-
+# If vertical --> Encode
+# if horizontal --> transform then encode
+# input
+# support pruning
+# insert in frequent itemsets
+# generate candidate itemsets C2
+#  ----- In Recursion -----
+# support pruning
+# insert in frequent itemsets
+# generate candidate itemsets Lk
+if __name__ == "__main__":
+    main()
 
 # Split the 'items' column into a list of items
-df['items'] = df['items'].str.split(',')
+# df['items'] = df['items'].str.split(',')
 
 # Explode the list of items into separate rows
-df = df.explode('items')
+# df = df.explode('items')
 
 # Group by 'items' and aggregate the transactions into lists for each item
-grouped_df = df.groupby('items')['TiD'].agg(list).reset_index()
+# grouped_df = df.groupby('items')['TiD'].agg(list).reset_index()
+
+# print(grouped_df)
 
 # Use TransactionEncoder to encode the 'TiD' column
-te = TransactionEncoder()
-encoded_data = te.fit_transform(
-    grouped_df['TiD'].apply(lambda x: [str(x)] if isinstance(x, int) else [str(i) for i in x]))
+# te = TransactionEncoder()
+# encoded_data = te.fit_transform(
+# grouped_df['TiD'].apply(lambda x: [str(x)] if isinstance(x, int) else [str(i) for i in x]))
 
 # Create a new DataFrame with the encoded 'TiD' data
-encoded_df = pd.concat([grouped_df['items'].reset_index(drop=True), pd.DataFrame(encoded_data, columns=te.columns_)],
-                       axis=1)
+# encoded_df = pd.concat([grouped_df['items'].reset_index(drop=True), pd.DataFrame(encoded_data, columns=te.columns_)],
+# axis=1)
 
-encoded_df.replace({True: 1, False: 0}, inplace=True)
+# encoded_df.replace({True: 1, False: 0}, inplace=True)
 
-min_sup = int(input("Enter min Support"))
-min_conf = int(input("Enter min Confidence"))
+# print(encoded_df)
 
-df_after_pruning = support_pruning(encoded_df, min_sup)
-insert_frequent_itemsets(df_after_pruning)
-df_after_generating = generate_frequent_itemsets(df_after_pruning)
+# min_sup = int(input("Enter min Support"))
+# min_conf = int(input("Enter min Confidence"))
 
-print(df_after_pruning)
-print(frequent_itemsets)
-print(df_after_generating)
+# type = ECLAT_implementation
+# df_after_pruning = ECLAT_implementation.support_pruning(encoded_df, min_sup)  # L1
+# ECLAT_implementation.insert_frequent_itemsets(df_after_pruning)
+# df_after_generating_l2 = ECLAT_implementation.generate_frequent_itemsets_l2(df_after_pruning)
+# df_after_pruning_l2 = ECLAT_implementation.support_pruning(df_after_generating_l2, min_sup)
+# ECLAT_implementation.insert_frequent_itemsets(df_after_pruning_l2)
+
+# combinations = ECLAT_implementation.generate_frequent_itemsets_lk(df_after_pruning_l2)
+# print(df_after_pruning)
+# print(frequent_itemsets)
+# print(df)
+# print(type)
+# print(combinations)
+
+# 1 --> support pruning
+# 2 --> insert
+# 3 --> generate
